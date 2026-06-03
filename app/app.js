904b6da3422871b19fa17c9fcc4c1072ab8bbe6a
harshitha <harshithareddy2606@gmail.com>
2026-06-03 11:52:16 +0100

    Add ecommerce application files

---
+++ app/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: 'mysql-db',
+  user: 'root',
+  password: 'root123',
+  database: 'ecommerce'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err);
+    return;
+  }
+  console.log('Connected to MySQL');
+});
+
+app.get('/products', (req, res) => {
+  db.query('SELECT * FROM products', (err, results) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    res.json(results);
+  });
+});
+
+app.listen(8080, () => {
+  console.log('Server running on port 8080');
+});
